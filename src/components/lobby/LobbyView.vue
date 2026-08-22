b943bbbe84b42cc54a2ff3f63cb9319d3decadda
BestGuo2020 <BestGuo2020@foxmail.com>
2026-08-22 12:04:58 +0800

    feat(lobby): rename subtitle to LIANHUA MAHJONG COLLECTIONS

--- src/components/lobby/LobbyView.vue
+++ src/components/lobby/LobbyView.vue
@@ -118,7 +118,7 @@ function closeDialog() {
 
 <template>
   <section class="lobby">
-    <p class="eyebrow">LINGNAN GUANGDONG MAHJONG</p>
+    <p class="eyebrow">LIANHUA MAHJONG COLLECTIONS</p>
     <h1>莲花<span>广麻</span></h1>
     <p class="subtitle">一款莲花县特有的地方麻将游戏玩法</p>
     <button v-if="storedSession && !roomId" class="continue-session" @click="$emit('resumeSession')">
